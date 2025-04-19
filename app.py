05f61bd6189115d9b5347ceecaa005912e0c1d77
vineethkumar3 <vineethboddu112233@gmail.com>
2025-04-19 13:33:18 -0400

    added amount

--- app.py
+++ app.py
@@ -12,15 +12,15 @@ def home():
     if 'user' not in session:
         return redirect(url_for('login'))
     books = [
-        {"id": 1, "title": "Atomic Habits", "image": "https://m.media-amazon.com/images/I/81eB+7+CkUL.jpg"},
-        {"id": 2, "title": "Rich Dad Poor Dad", "image": "https://m.media-amazon.com/images/I/91bYsX41DVL.jpg"},
-        {"id": 3, "title": "The Alchemist", "image": "https://m.media-amazon.com/images/I/71aFt4+OTOL.jpg"},
-        {"id": 4, "title": "Harry Potter", "image": "https://m.media-amazon.com/images/I/81iqZ2HHD-L.jpg"},
-        {"id": 5, "title": "To Kill a Mockingbird", "image": "https://m.media-amazon.com/images/I/710jnzKlDTL.jpg"},
-        {"id": 6, "title": "Think and Grow Rich", "image": "https://m.media-amazon.com/images/I/81WcnNQ-TBL.jpg"},
-        {"id": 7, "title": "The Power of Now", "image": "https://m.media-amazon.com/images/I/71UwSHSZRnS.jpg"},
-        {"id": 8, "title": "The Subtle Art...", "image": "https://m.media-amazon.com/images/I/91uwocAMtSL.jpg"},
-        {"id": 9, "title": "Ikigai", "image": "https://m.media-amazon.com/images/I/71KilybDOoL.jpg"}
+        {"id": 1, "title": "Atomic Habits", "image": "https://m.media-amazon.com/images/I/81eB+7+CkUL.jpg","price":12.99},
+        {"id": 2, "title": "Rich Dad Poor Dad", "image": "https://m.media-amazon.com/images/I/91bYsX41DVL.jpg","price":10.99},
+        {"id": 3, "title": "The Alchemist", "image": "https://m.media-amazon.com/images/I/71aFt4+OTOL.jpg","price":9.99},
+        {"id": 4, "title": "Harry Potter", "image": "https://m.media-amazon.com/images/I/81iqZ2HHD-L.jpg","price":11.99},
+        {"id": 5, "title": "To Kill a Mockingbird", "image": "https://m.media-amazon.com/images/I/710jnzKlDTL.jpg","price":12.99},
+        {"id": 6, "title": "Think and Grow Rich", "image": "https://m.media-amazon.com/images/I/81WcnNQ-TBL.jpg","price":5.99},
+        {"id": 7, "title": "The Power of Now", "image": "https://m.media-amazon.com/images/I/71UwSHSZRnS.jpg","price":15.99},
+        {"id": 8, "title": "The Subtle Art...", "image": "https://m.media-amazon.com/images/I/91uwocAMtSL.jpg","price":10.99},
+        {"id": 9, "title": "Ikigai", "image": "https://m.media-amazon.com/images/I/71KilybDOoL.jpg","price":6.99}
     ]
 
     return render_template('home.html',books=books)
@@ -36,6 +36,15 @@ def login():
         else:
             return render_template('login.html', error='Invalid Credentials')
     return render_template('login.html')
+
+@app.route('/cart')
+def cart():
+    return render_template('cart.html')
+
+@app.route('/checkout')
+def checkout():
+    return render_template('checkout.html')
+
 
 @app.route('/logout')
 def logout():
